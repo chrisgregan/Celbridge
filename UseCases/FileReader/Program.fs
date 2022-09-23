a4fbb1a54762449f2504ae8849a9546ce24c788f
Chris Gregan <chrisgregan@gmail.com>
2022-09-23 18:05:44 +0100

    Added Excel file reader use case

---
+++ UseCases/FileReader/Program.fs
@@ -0,0 +1,17 @@
+﻿open FSharp.Interop.Excel
+
+type ExcelType = ExcelFile<"File.xlsx">
+
+let readRows =
+    let file = new ExcelType()
+    file.Data 
+    |> Seq.toArray
+
+let validateExcel maxValue =
+    readRows
+    |> Seq.iter (fun row -> 
+        let v : float = row.Score
+        if v > maxValue then
+            failwith "Value out of range")
+
+validateExcel 100    
